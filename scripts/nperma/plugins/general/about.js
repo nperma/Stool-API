fc1855af7bd6c205ec947c3a735c584feace86aa
Nperma <129764133+nperma@users.noreply.github.com>
2024-08-24 19:42:28 +0700

    update scripts/nperma/plugins/general/about.js

---
+++ scripts/nperma/plugins/general/about.js
@@ -0,0 +1,12 @@
+let handler = function (ev, { sender, config }) {
+    let txt = "§6ABOUT - TOOLS ]-------------------------------------\n";
+    txt += "§e+ Type: §cScriptAPI§4(§aEssentials§4)§r\n";
+    txt += `§e+ Version: §a${config.version}§r\n`;
+    txt += `§e+ Creator: §g@Nperma\n`;
+    return sender.sendMessage(txt);
+};
+
+handler.commands = handler.helps = ["about"]
+handler.category = "general";
+
+export default handler
