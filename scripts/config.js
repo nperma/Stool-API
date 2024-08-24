d9a2693f74722833ab06d4ace2f69b6869ea280d
Nperma <129764133+nperma@users.noreply.github.com>
2024-08-24 19:32:36 +0700

    update scripts/config.js

---
+++ scripts/config.js
@@ -0,0 +1,144 @@
+const config = {
+    /** 
+  # DONT CHANGE THIS!!
+  @dev_spot */
+    version: "1.0.5",
+    creator: "Nperma",
+    github: "https://github.com/nperma",
+
+    /** @configuration */
+
+    prefix: ["+", "-", "?", "!"], //prefix default command
+    admin_tag: "perm.admin", //admin tag
+    log: {
+        message: true,
+        teleport: true
+    },
+    default_title: "§l§2[§aSERVERTOOL§l§2]§r §7»§r ",
+    default_balance: 2000,
+    numeric_sysmbol: ["", "k", "M", "B", "T"],
+    home: {
+        countdown: 3, //3second
+        homelimit: {
+            /** @type {Object} @property {Tag} key @property {limithome} value */
+            default: 2,
+            "perm.admin": 20,
+            vip: 5,
+            mvp: 8,
+            legend: 12
+        }
+    },
+    warp: {
+      countdown: 3,
+      teleportbroadcast: true //will sent teleport to all player
+    }
+};
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+export default config;
