b24a990daad4c6d07ad2a3a26f278d4ca24652ed
Nperma <129764133+nperma@users.noreply.github.com>
2024-08-24 23:26:43 +0700

    update scripts/nperma/plugins/_log/message.js

---
+++ scripts/nperma/plugins/_log/message.js
@@ -0,0 +1,17 @@
+let handler = ev => ev;
+
+handler.after = (ev, { config, usePlugin: plugin, prefix, command }) => {
+    if (!config.log.message) return;
+    console.log(`\nSENDER: ${ev.sender.name}\nUSE-PL: ${
+        plugin ? true : false
+    }\n${
+        plugin
+            ? `plugin: ${plugin}\ncommand: ${command}\nprefix: ${
+                  prefix ?? "none"
+              }\n`
+            : ""
+    }message: ${ev.message}
+  `);
+};
+
+export default handler;
