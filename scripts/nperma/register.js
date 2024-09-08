3e5ff83a617381d2a5cfadc88025268cbd5d8a35
Nperma <129764133+nperma@users.noreply.github.com>
2024-09-08 18:25:34 +0700

    Create register.js

---
+++ scripts/nperma/register.js
@@ -0,0 +1,33 @@
+export default [
+    /** @general */
+    "help-general",
+    "tps-general",
+    "about-general",
+    "home-general",
+    "sb-general",
+    "clearchat-general",
+    "warp-general",
+    "land-general",
+    "mods-general",
+    "online-general",
+    /** @admin */
+    "warp-admin",
+    "invsee-admin",
+    "msb-admin",
+    "teleport-admin",
+    /** @economy */
+    "balance-economy",
+    "pay-economy",
+    /** @logger */
+    "message-_log",
+    "detect-_log",
+    /** @system */
+    "join-_system",
+    // "player-_system",
+    //  "server-_system",
+    // "ui-_system",
+    // "chat-_system",
+    /** @developer */
+    "eval-dev",
+    "resetdb-dev"
+];
